343a0aa7de25bcfe4bd7bf2502df11f4bf1e8e14
bipasnadulal <bipasna.dulal75@gmail.com>
2026-08-24 13:41:49 +0545

    initialized the project

---
+++ OOP/upload_to_pypi/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="probability_lab",
+    version="1.1",
+    description="Gaussian and Binomial distributions",
+    packages=["probability_lab"],
+    author="Bipasna Dulal",
+    author_email="bipansa.dulal75@gmail.com",
+    zip_safe=False,
+)
